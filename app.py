110183782228eb4e30999c4271e5ba04276d8e72
siddesh5467 <yemtellusiddesh@gmail.com>
2026-03-29 16:44:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as numpy
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.set_page_config(page_title="Trader Behavior vs Sentiment", layout="wide")
